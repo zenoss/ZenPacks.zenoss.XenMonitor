ba4c8bf8bfd2916b1cbb44a58d78af6c4c485308
cholden <cholden@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-11-17 19:35:12 +0000

    fix typo; fix migrate script; add copyrights reviewed by bedwards refs US1071/US1072
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.XenMonitor@29271 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/XenMonitor/migrate/ChangeXenPluginName.py
+++ ZenPacks/zenoss/XenMonitor/migrate/ChangeXenPluginName.py
@@ -1,8 +1,15 @@
-######################################################################
+###########################################################################
 #
-# Copyright 2008 Zenoss, Inc.  All Rights Reserved.
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
 #
-######################################################################
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 
 import Globals
 from Products.ZenModel.migrate.Migrate import Version
@@ -20,8 +27,12 @@ class ChangeXenPluginName(ZenPackMigration):
         vhmDeviceClass = 'Virtual Machine Host'
         deviceClass = pack.dmd.Devices.Server._getOb(vhmDeviceClass).Xen
         collectorPlugins = list( deviceClass.zCollectorPlugins )
-        newPluginList = [ plugin == fromPluginName and toPluginName or plugin
-                          for plugin in collectorPlugins ]
+        newPluginList = []
+        for plugin in collectorPlugins:
+            if plugin == fromPluginName:
+                newPluginList.append( toPluginName )
+            else:
+                newPluginList.append( plugin )
         if newPluginList != collectorPlugins:
             deviceClass.setZenProperty( 'zCollectorPlugins',
                                         tuple( newPluginList ) )
